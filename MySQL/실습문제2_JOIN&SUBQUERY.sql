910feeaff5ceef9f2fb9bcfdf7cd321c9b66cb31
yogu970126 <yogu970126@gmail.com>
2024-01-02 22:58:33 +0900

    MySQL 실습문제2
    5번 풀이 추가

--- MySQL/실습문제2_JOIN&SUBQUERY.sql
+++ MySQL/실습문제2_JOIN&SUBQUERY.sql
@@ -1,19 +1,5 @@
 -- Sakila : DVD 대여 및 영화 정보와 관련된 데이터 포함
 
-SELECT * FROM category; -- 영화 카테고리 정보 : category_id
-SELECT * FROM rental; -- DVD 대여 정보 : customer_id, inventory_id
-SELECT * FROM inventory; -- DVD 대여점에서 관리하는 정보 : inventory_id, film_id
-SELECT * FROM customer; -- DVD 대여 고객 정보 : customer_id, address_id
-SELECT * FROM film; -- 영화 정보 : film_id
-SELECT * FROM actor; -- 영화 배우 정보 : actor_id
-
-SELECT * FROM film_category; -- film과 category 연결 : film_id, category_id
-SELECT * FROM film_actor; -- film과 actor 연결 : film_id, actor_id
-
-SELECT * FROM address; -- 고객 주소 정보 : address_id, city_id, address, district
-SELECT * FROM city; -- city_id, country_id, city
-SELECT * FROM country; -- country_id, country
-
 -- 1. customer 테이블의 first_name이 TRACY인 사람들 조회 
 SELECT country, city, address ,district, first_name, last_name
 FROM customer
@@ -73,10 +59,39 @@ FROM actor
 	JOIN film_actor USING(actor_id)
 	JOIN film USING(film_id)
 WHERE title = 'NOON PAPI';
+
+
+SELECT * FROM category; -- 영화 카테고리 정보 : category_id
+SELECT * FROM rental; -- DVD 대여 정보 : customer_id, inventory_id
+SELECT * FROM inventory; -- DVD 대여점에서 관리하는 정보 : inventory_id, film_id
+SELECT * FROM customer; -- DVD 대여 고객 정보 : customer_id, address_id
+SELECT * FROM film; -- 영화 정보 : film_id
+SELECT * FROM actor; -- 영화 배우 정보 : actor_id
+
+SELECT * FROM film_category; -- film과 category 연결 : film_id, category_id
+SELECT * FROM film_actor; -- film과 actor 연결 : film_id, actor_id
+
+SELECT * FROM address; -- 고객 주소 정보 : address_id, city_id, address, district
+SELECT * FROM city; -- city_id, country_id, city
+SELECT * FROM country; -- country_id, country
+
+
 -- 4. 각 카테고리별 이메일이 JOYCE.EDWARDS@sakilacustomer.org인 고객이 빌린 DVD 대여 수 조회 
--- id 검색
-SELECT * FROM customer
-WHERE email = 'JOYCE.EDWARDS@sakilacustomer.org';
+-- id 검색  
 
 
 -- 5. 이메일이 JOYCE.EDWARDS@sakilacustomer.org인 고객이 가장 최근에 빌린 영화 제목과 영화 내용을 조회
+-- JOYCE.EDWARDS@sakilacustomer.org 가 빌린 영화 렌탈 목록
+SELECT title, description, rental_date
+FROM film
+JOIN inventory USING(film_id)
+JOIN rental  USING(inventory_id)
+JOIN customer USING(customer_id)
+WHERE email ='JOYCE.EDWARDS@sakilacustomer.org'
+ORDER BY 3 DESC
+LIMIT 1;
+
+
+
+
+
