93c5b1647d6375d0c4221b51dad5034ef49b950d
yogu970126 <yogu970126@gmail.com>
2024-01-05 17:33:09 +0900

    MySQL 실습문제3
    MySQL 실습문제3 약간수정

--- MySQL/실습문제3_CREATE.sql
+++ MySQL/실습문제3_CREATE.sql
@@ -1,6 +1,11 @@
 -- 실습 문제
 -- 도서관리 프로그램을 만들기 위한 테이블 만들기
 
+DROP TABLE tb_rent;
+DROP TABLE tb_member;
+DROP TABLE tb_book;
+DROP TABLE tb_publisher;
+
 -- 테이블 담을 Library 스키마 생성
 CREATE SCHEMA Library;
 
@@ -20,12 +25,6 @@ INSERT INTO tb_publisher VALUES(2, '골든래빗', '0505-398-0505');
 INSERT INTO tb_publisher VALUES(3, '천그루숲', '031-955-3777');
 
 SELECT * FROM tb_publisher;
--- 해설 -------------------------------
-CREATE TABLE tb_publisher(
-	pub_no INT AUTO_INCREMENT PRIMARY KEY,
-    pub_name VARCHAR(20) NOT NULL,
-    phone VARCHAR(13) 
-);
 
 -- 2. 도서들에 대한 데이터를 담기 위한 도서 테이블 (tb_book)
 --    컬럼 : bk_no (도서번호) -- 기본 키
@@ -43,8 +42,6 @@ CREATE TABLE tb_book (
     bk_pub_no INT,
     FOREIGN KEY(bk_pub_no) REFERENCES tb_publisher(pub_no) ON DELETE CASCADE
 );
-ALTER TABLE tb_book ADD CONSTRAINT FOREIGN KEY (pub_no) REFERENCES tb_publisher(pub_no);
-
 INSERT INTO tb_book VALUES(1, '오늘부터 개발자', '김병욱', 16800, 1);
 INSERT INTO tb_book VALUES(2, '요즘 우아한 개발', '우아한 형제들', 24000, 2);
 INSERT INTO tb_book VALUES(3, '프로덕트 매니저 원칙', '장홍석', 22000, 2);
@@ -52,15 +49,6 @@ INSERT INTO tb_book VALUES(4, '코딩 좀 아는 사람', '제러미 키신', 17
 INSERT INTO tb_book VALUES(5, '그렇게 쓰면 아무도 안 읽습니다', '전주경', 19800, 3);
 
 SELECT * FROM tb_book;
-
--- 해설 -------------------------------
-CREATE TABLE tb_book(
-	bk_no INT AUTO_INCREMENT PRIMARY KEY,
-    bk_title VARCHAR(50) NOT NULL,
-    bk_author VARCHAR(20) NOT NULL,
-    bk_price INT,
-    pub_no INT
-);
 
 -- 3. 회원에 대한 데이터를 담기 위한 회원 테이블 (tb_member)
 --    컬럼 : member_no(회원번호) -- 기본 키
@@ -94,19 +82,6 @@ VALUES(3, 'user03', 'pass03', '강길순', 'F', '경기도 광주시', '010-4444
 
 SELECT * FROM tb_member;
 
--- 해설 -------------------------------
-CREATE TABLE tb_member(
-	member_no INT AUTO_INCREMENT PRIMARY KEY,
-    member_id VARCHAR(30) UNIQUE NOT NULL,
-    member_pwd VARCHAR(30) NOT NULL,
-    member_name VARCHAR(30) NOT NULL,
-    gender VARCHAR(1) CHECK(gender IN('M','F')) NOT NULL,
-    address VARCHAR(20),
-    phone VARCHAR(20),
-    status VARCHAR(1) CHECK(status IN ('Y', 'N')) DEFAULT 'N',
-    enroll_date DATE DEFAULT(current_date)    
-);
-
 -- 4. 도서를 대여한 회원에 대한 데이터를 담기 위한 대여 목록 테이블(tb_rent)
 --    컬럼 : rent_no(대여번호) -- 기본 키
 --           rent_mem_no(대여 회원번호) -- 외래 키(tb_member와 참조)
@@ -122,10 +97,6 @@ CREATE TABLE tb_rent(
     FOREIGN KEY(rent_mem_no) REFERENCES tb_member(member_no) ON DELETE SET NULL,
     FOREIGN KEY(rent_book_no) REFERENCES tb_book(bk_no) ON DELETE SET NULL
 );
-ALTER TABLE tb_rent ADD CONSTRAINT member_no_fk
-	FOREIGN KEY(member_no) REFERENCES tb_member(member_no);
-ALTER TABLE tb_rent ADD CONSTRAINT bk_no_fk
-	FOREIGN KEY(bk_no) REFERENCES tb_book(bk_no);
 
 INSERT INTO tb_rent VALUES(1, 1, 2, default);
 INSERT INTO tb_rent VALUES(2, 1, 3, default);
@@ -134,14 +105,6 @@ INSERT INTO tb_rent VALUES(4, 2, 2, default);
 INSERT INTO tb_rent VALUES(5, 1, 5, default);
 
 SELECT * FROM tb_rent;
-
--- 해설 -------------------------------
-CREATE TABLE tb_rent(
-	rent_no INT AUTO_INCREMENT PRIMARY KEY,
-    member_no INT, 
-    bk_no INT, 
-    rent_date DATE DEFAULT(current_date)
-);
 
 -- 5. 2번 도서를 대여한 회원의 이름, 아이디, 대여일, 반납 예정일(대여일 + 7일)을 조회하시오.
 SELECT member_name as '회원 이름', member_id as '아이디', 
