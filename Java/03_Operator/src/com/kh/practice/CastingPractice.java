df3dc7a9fce76f006574bf28047f0c9a5d9f90cd
yogu970126 <yogu970126@gmail.com>
2024-01-11 15:25:17 +0900

    자바 02_변수 실습문제
    자바 02_변수 실습문제

--- Java/03_Operator/src/com/kh/practice/CastingPractice.java
+++ Java/03_Operator/src/com/kh/practice/CastingPractice.java
@@ -8,8 +8,8 @@ public class CastingPractice {
 
 	public static void main(String[] args) {
 	CastingPractice cp = new CastingPractice();
-	cp.method1();
-	//cp.method2();
+	//cp.method1();
+	cp.method2();
 
 
 	}
@@ -18,34 +18,28 @@ public class CastingPractice {
 		System.out.print("문자 : ");
 		char ch = sc.nextLine().charAt(0);
 		int word = ch;
-		System.out.printf("%c"word);
-		System.out.printf(word+1);
-		
-		
-		
-		
+		System.out.println(ch + " unicode : " + word);
+		System.out.println((char)(ch+1) + " unicode : " + (word+1));		
 	}
 	
 	public void method2() {
 		// 국어, 영어, 수학 세 과목의 점수를 입력 받아 총점과 평균을 출력하세요.
-		int kor = 0;
-		int eng = 0;
-		int math = 0;
-		
 		
 		System.out.print("국어 점수 : ");
-		kor = sc.nextInt();
+		double kor = sc.nextInt();
 		
 		System.out.print("영어 점수 : ");
-		eng = sc.nextInt();
+		double eng = sc.nextInt();
 		
 		System.out.print("수학 점수 : ");
-		math = sc.nextInt();
+		double math = sc.nextInt();
 		
-		System.out.println("국어 : " + kor);
-		System.out.println("영어 : " + eng);
-		System.out.println("수학 : " + math);
-		System.out.println("총점 : " + (kor + eng + math));
-		System.out.println("평균 : " + (kor + eng + math) / 3);	
+		double avg = (kor + eng + math) / 3;
+		
+		System.out.println("국어 : "+ (int)kor);
+		System.out.println("영어 : "+ (int)eng);
+		System.out.println("수학 : "+ (int)math);
+		System.out.println("총점 : "+ (int)(kor + eng + math));
+		System.out.printf("평균 : %.2f", avg);	
 	}
 }
