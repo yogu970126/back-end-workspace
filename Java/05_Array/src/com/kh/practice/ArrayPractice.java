98050c6cd2af1a4e69c8bcf4000af312780402be
yogu970126 <yogu970126@gmail.com>
2024-01-21 22:39:18 +0900

    자바 05_배열 실습문제
    자바 05_배열 실습문제 다 풀었음

--- Java/05_Array/src/com/kh/practice/ArrayPractice.java
+++ Java/05_Array/src/com/kh/practice/ArrayPractice.java
@@ -1,13 +1,30 @@
 package com.kh.practice;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayPractice {
+	Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+	ArrayPractice ap = new ArrayPractice();
+	
+//	ap.method1();
+//	ap.method2();
+//	ap.method3();
+//	ap.method4();
+	ap.method5();
+	
+	}
 	
 	/*
 	 * 길이가 5인 배열을 선언하고 과일 5개로 초기화 한 후 본인이 좋아하는 과일 하나를 출력하세요. (과일 5개는 뭐든지~)
 	 * 
 	 * */
 	public void method1() {
-		
+		String[] fruit = {"귤", "사과", "배", "방울토마토", "한라봉"};
+		int ran = (int)(Math.random() * 5);
+		System.out.println(fruit[ran]);
 	}
 	
 	/*
@@ -24,6 +41,20 @@ public class ArrayPractice {
 	 * 2
 	 * */
 	public void method2() {
+		System.out.print("정수 : ");
+		int arrayLength = Integer.parseInt(sc.nextLine());
+		int[] num = new int[arrayLength];
+		int sum = 0;
+		
+		for(int i = 0; i < arrayLength; i++) {
+		
+			System.out.print("배열 " + (i+1) + "번째 인덱스에 넣을 값 : ");
+			num[i] = Integer.parseInt(sc.nextLine());
+			sum += num[i];
+		}
+		System.out.println(Arrays.toString(num));
+		System.out.println(sum);
+		
 		
 	}
 	
@@ -34,7 +65,22 @@ public class ArrayPractice {
 	 * 
 	 * */
 	public void method3() {
+		System.out.print("메뉴판 음식의 개수 : ");
+		int arrayLength = Integer.parseInt(sc.nextLine());
+		String[] menu = new String[arrayLength];
 		
+		for(int i = 0; i < arrayLength; i++) {
+			System.out.print((i+1) + "번째 메뉴 : ");
+			menu[i] = sc.nextLine();
+		}
+		
+		System.out.print("주문할 음식을 입력하세요 : ");
+		String order = sc.nextLine();
+		for(int i = 0; i < arrayLength; i++) {
+			if(menu[i].equals(order)) {
+				System.out.println(menu[i] + " 배달 가능");
+			} 
+		}
 	}
 	
 	/*
@@ -47,7 +93,18 @@ public class ArrayPractice {
 	 * 
 	 * */
 	public void method4() {
+		System.out.print("주민등록번호 : ");
+		String no = sc.nextLine();
+		char[] noArr = new char[no.length()];
 		
+		for(int i = 0; i < no.length(); i++) {
+			noArr[i] = no.charAt(i);
+			if(i < 8) {
+				System.out.print(noArr[i]);				
+			} else if(i >= 8) {
+				System.out.print("*");
+			}
+		}
 	}
 	
 	/*
@@ -58,7 +115,14 @@ public class ArrayPractice {
 	 * 
 	 * */
 	public void method5() {
+		System.out.print("단어 입력 : ");
+		String str = sc.nextLine();
+		char[] revStr = new char[str.length()];
 		
+		for(int i = 0; i < str.length(); i++) {
+			revStr[i] = str.charAt(-(i - (str.length()-1)));
+		}
+		System.out.print(revStr);
 	}
 	
 }
