5f58f5089ec2cbfb91f01b1f4b5d2337cb9f2557
yogu970126 <yogu970126@gmail.com>
2024-01-25 17:36:52 +0900

    자바 11_컬렉션 Set
    - Set 연습에 사용할 모델 생성

---
+++ Java/11_Collection/src/com/kh/set/model/Person.java
@@ -0,0 +1,65 @@
+package com.kh.set.model;
+
+import java.util.Objects;
+
+// TreeSet은 Comparable이 없기 때문에 재정의 해줘야함
+public class Person implements Comparable<Person>{
+	
+	private String name;
+	private int age;
+	
+	public Person() {
+	}
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public int compareTo(Person o) {
+		return this.name.compareTo(o.name) * -1;
+//		return (this.age - o.age) * -1;
+	}
+	
+	
+
+}
