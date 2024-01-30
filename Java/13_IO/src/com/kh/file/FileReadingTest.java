4d0af08ecded4cdcca823f5b30fe705fcada0aab
yogu970126 <yogu970126@gmail.com>
2024-01-30 11:52:06 +0900

    자바 13_입출력
    자바 13_입출력
    - 문자 기반 스트림

---
+++ Java/13_IO/src/com/kh/file/FileReadingTest.java
@@ -0,0 +1,17 @@
+package com.kh.file;
+
+/*
+ * 문자 기반 스트림
+ * - 문자 데이터를 읽고 출력할 때 사용
+ * - Reader, Writer : 문자 기반 입출력 스트림의 최상위 클래스
+ * */
+
+public class FileReadingTest {
+	
+	String fileName = "";
+
+	public static void main(String[] args) {
+		
+	}
+
+}
