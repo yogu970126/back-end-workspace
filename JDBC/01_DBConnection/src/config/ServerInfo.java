b523be2298eedef2298e7af9303d4040774cf4be
yogu970126 <yogu970126@gmail.com>
2024-02-01 17:28:45 +0900

    JDBC 01_연결 연습3
    JDBC 01_연결 연습3
    - 상수로 지정한 정보들 별도 파일로 관리하기
    - 상수이기 때문에 인터페이스로 생성

---
+++ JDBC/01_DBConnection/src/config/ServerInfo.java
@@ -0,0 +1,9 @@
+package config;
+
+public interface ServerInfo {
+	String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	String URL = "jdbc:mysql://localhost:3306/kh";
+	String USER = "root";
+	String PASSWORD = "1234";
+
+}
