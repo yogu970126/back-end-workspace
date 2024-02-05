d6b93e1427eabb868692d127b3c356a77c5df06a
yogu970126 <yogu970126@gmail.com>
2024-02-05 17:09:21 +0900

    JDBC 03_실습문제 풀이

--- JDBC/03_MemberMVC/src/com/kh/Application.java
+++ JDBC/03_MemberMVC/src/com/kh/Application.java
@@ -8,21 +8,25 @@ import com.kh.controller.MemberController;
 import com.kh.model.Member;
 
 public class Application {
-	Scanner sc = new Scanner(System.in);
-	MemberController mc = new MemberController();
+	private Scanner sc = new Scanner(System.in);
+	private MemberController mc = new MemberController();
 	Member m = new Member();
 		
 	public static void main(String[] args) {
 		Application app = new Application();
-		app.mainMenu();
+		try {
+			app.mainMenu();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public void mainMenu() {
+	public void mainMenu() throws SQLException {
 		
 		System.out.println("===== KH 사이트 =====");
 		boolean check = true;
-		try {
+		
 			while(check) {
 				System.out.println("****** 메인 메뉴 ******");
 				System.out.println("1. 회원가입");
@@ -43,19 +47,10 @@ public class Application {
 					break;
 				}
 			}
-		} catch(SQLException e) {
-			System.out.println(e.getMessage());
-			mainMenu();
-		} catch(NumberFormatException e) {
-			System.out.println("올바른 메뉴를 입력해주세요");
-			mainMenu();
-		}
-	}
+		} 
 		
 	public void signUp(Member m) throws SQLException {
 		// 아이디, 비밀번호, 이름을 사용자에게 입력받아 MemberController의 signUp 메서드 반환 결과에 따라
-		// true면 "성공적으로 회원가입 완료하였습니다." 출력
-		// false면 "중복된 아이디입니다. 다시 입력해주세요." 출력
 		System.out.print("아이디 입력 : ");
 		String id = sc.nextLine();
 		
@@ -69,22 +64,44 @@ public class Application {
 		m.setPassword(password);
 		m.setName(name);
 		
-		if(mc.signUp(m) == true) {
-			System.out.println(m.getName() + "님 회원가입 완료 되었습니다");
-		};
-		
-		
+		if(mc.signUp(m)) {
+			// true면 "성공적으로 회원가입 완료하였습니다." 출력
+			System.out.println(m.getName() + "님 성공적으로 회원가입 완료하였습니다.");
+		} else {
+			// false면 "중복된 아이디입니다. 다시 입력해주세요." 출력
+			System.out.println("중복된 아이디입니다. 다시 입력해주세요.");
+		}
 	}
 	
-	public void login() {
+	public void login() throws SQLException {
 		// 아이디, 비밀번호, 이름을 사용자에게 입력받아 MemberController의 login 메서드 반환 결과를 이름으로 받고
 		// 이름이 null아 아니면 "~~님, 환영합니다!" 출력
 		//   -->> login 성공했다면 memberMenu() 호출
 		// null이면 "틀린 아이디 또는 비밀번호입니다. 다시 입력해주세요." 출력
+		System.out.println("****** 로그인 ******");
+		System.out.print("아이디 : ");
+		String id = sc.nextLine();
+		
+		System.out.print("패스워드 : ");
+		String password = sc.nextLine();
+		
+		// MemberController의 login 메서드 반환 결과를 이름으로 받고
+		String name = mc.login(id, password);
+		
+		if(name != null) {
+			System.out.println(name + "님, 환영합니다.");
+			// -->> login 성공했다면 memberMenu() 호출
+			memberMenu();
+		} else {
+			// null이면 "틀린 아이디 또는 비밀번호입니다. 다시 입력해주세요." 출력
+			System.out.println("틀린 아이디 또는 비밀번호입니다. 다시 입력해주세요");
+		}
+		
+		
 		
 	}
 	
-	public void memberMenu() {
+	public void memberMenu() throws SQLException {
 		boolean check = true;
 		while(check) {
 			System.out.println("****** 회원 메뉴 ******");
@@ -101,26 +118,70 @@ public class Application {
 				break;
 			case 3:
 				// 로그아웃 처리 -> 메인 메뉴로 리턴
+				System.out.println("로그아웃 실행");
+				check = false;
 				break;
 			}
 		}
 	}
 	
-	public void changePassword() {
+	public void changePassword() throws SQLException {
 		// 아이디, 현재 비밀번호, 새로운 비밀번호를 사용자한테 입력받아
+		System.out.print("아이디 : ");
+		String id = sc.nextLine();
+		
+		System.out.print("현재 비밀번호 : ");
+		String password = sc.nextLine();
+		
+		System.out.print("새로운 비밀번호 : ");
+		String newPassword = sc.nextLine();
+		
 		// MemberController의 changePassword 메서드 반환 결과에 따라
-		// true면 "비밀번호 변경에 성공했습니다." 출력
-		// false면 "비밀번호 변경에 실패했습니다. 다시 입력해주세요." 출력
+		if(mc.changePassword(id, password, newPassword)) {
+			// true면 "비밀번호 변경에 성공했습니다." 출력
+			System.out.println("비밀번호 변경에 성공했습니다.");
+		} else {
+			// false면 "비밀번호 변경에 실패했습니다. 다시 입력해주세요." 출력
+			System.out.println("비밀번호 변경에 실패했습니다.");
+		}
 		
 	}
 	
-	public void changeName() {
+	public void changeName() throws SQLException {
 		// 아이디, 비밀번호를 사용자에게 입력받아 사용자가 맞는지 확인 후 - memberController의 login 메서드 활용
+		System.out.print("아이디 : ");
+		String id = sc.nextLine();
+		
+		System.out.print("비밀번호 : ");
+		String password = sc.nextLine();
+		
+		String name = mc.login(id, password);
 		// 이름이 null이 아니면 --> "현재 설정된 이름 : OOO" 출력
-		//				   --> 변경할 이름을 사용자한테 입력받아 
-		//					   MemberController의 changeName 메서드로 이름 변경후 
-		//					   "이름 변경에 성공하였습니다." 출력
-		// 이름이 null 이면 "이름 변경에 실패했습니다. 다시 입력해주세요." 출력
+		if(name != null) {
+			System.out.println("현재 설정된 이름 : " + name);
+			
+        //	--> 변경할 이름을 사용자한테 입력받아 
+			System.out.print("변경할 이름 : ");
+			String newName = sc.nextLine();
+			
+			if(newName != "") {
+		// MemberController의 changeName 메서드로 이름 변경후 
+		// "이름 변경에 성공하였습니다." 출력
+			mc.changeName(id, newName);
+			System.out.println("이름 변경에 성공하였습니다.");
+			} else {
+				// 이름이 null 이면 "이름 변경에 실패했습니다. 다시 입력해주세요." 출력
+				System.out.println("이름 변경에 실패하였습니다.");
+			}
+		} 
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 	
--- JDBC/03_MemberMVC/src/com/kh/controller/MemberController.java
+++ JDBC/03_MemberMVC/src/com/kh/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.kh.controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.kh.model.Member;
@@ -22,43 +23,113 @@ public class MemberController {
 	
 	// DB 연결 메서드
 	public Connection getConnect() throws SQLException {
-		Connection conn = DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASSWORD);
-		return conn;
+		return  DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASSWORD);
+	}
+	// 자원반납 메서드
+	public void close(PreparedStatement ps, Connection conn) throws SQLException {
+		if(ps != null) ps.close();
+		if(conn != null) conn.close();
+	}
+	
+	public void close(ResultSet rs ,PreparedStatement ps, Connection conn) throws SQLException {
+		if(rs != null) rs.close();
+		close(ps, conn);
 	}
 	
 	
 	public boolean signUp(Member m) throws SQLException {
-		//회원가입 기능 구현 -> member 테이블에 데이터 추가 (INSERT)
+		// 회원가입 기능 구현 -> member 테이블에 데이터 추가 (INSERT)
+		// -> 아이디가 기존에 있는지 체크 여부!
 		// login 메서드 활용해서 null이 아닐 때만 해당 코드 실행
+		
 		Connection conn = getConnect();
-		String query = "INSERT INTO member VALUES( ?, ?, ?)";
-		PreparedStatement ps = conn.prepareStatement(query);
 		
-		ps.setString(1, m.getId());
-		ps.setString(2, m.getPassword());
-		ps.setString(3, m.getName());
-		
-		int result = ps.executeUpdate();
-		if(result == 1) {
-			return true;
+		// 아이디체크 먼저 실행
+		// 입력받은 아이디가 존재하지 않으면 쿼리문 실행해 회원가입 진행
+		if(!idCheck(m.getId())) {
+			String query = "INSERT INTO member VALUES( ?, ?, ?)";
+			PreparedStatement ps = conn.prepareStatement(query);
+			
+			ps.setString(1, m.getId());
+			ps.setString(2, m.getPassword());
+			ps.setString(3, m.getName());
+			
+			int result = ps.executeUpdate();
+			
+			// 자원반납
+			close(ps,conn);
+			
+			if(result == 1) {
+				return true;
+			}
 		}
 		return false;
 	}
 	
-	public String login(String id, String password) {
-		// 로그인 기능 구현 -> member 테이블에서 id와 password로 멤버 정보 하나 가져오기 (SELECT)
-		return null;
-	}
-	
-	public boolean changePassword(String id, String oldPw, String newPw) {
-		// 비밀번호 변경 기능 구현 
-		// -> login 메서드 활용 후 사용자 이름이 null이 아니면 해당 UPDATE문 구현!
-		// -> member 테이블에서 id로 새로운 패스워드로 변경 (UPDATE)
+	public boolean idCheck(String id) throws SQLException {
+		Connection conn = getConnect();
+		String query = "SELECT id FROM member WHERE id = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1, id);
+		ResultSet rs = ps.executeQuery();
+		
+		String checkId = null;
+		if(rs.next()) {
+			checkId = rs.getString("id");
+		}
+		close(ps, conn); // 리턴 전에 닫아주면 된다
+		if(checkId != null) {
+			return true;
+		}
+		
 		return false;
 	}
 	
-	public void changeName(String id, String changeName) {
+	public String login(String id, String password) throws SQLException {
+		Connection conn = getConnect();
+		// 로그인 기능 구현 -> member 테이블에서 id와 password로 멤버 정보 하나 가져오기 (SELECT)
+		String query = "SELECT name FROM member WHERE id = ? AND password = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1, id);
+		ps.setString(2, password);
+		ResultSet rs = ps.executeQuery();
+		String name = null;
+		if(rs.next()) name = rs.getString("name");
+		close(rs, ps, conn);
+		return name;
+	}
+	
+	public boolean changePassword(String id, String oldPw, String newPw) throws SQLException {
+		Connection conn = getConnect();
+		// 비밀번호 변경 기능 구현 
+		// -> login 메서드 활용 후 사용자 이름이 null이 아니면 해당 UPDATE문 구현!
+		PreparedStatement ps = null;
+		boolean result = false;
+		if(login(id, oldPw) != null) {
+			// -> member 테이블에서 id로 새로운 패스워드로 변경 (UPDATE)
+			String query = "UPDATE member SET password = ? WHERE id = ?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, newPw);
+			ps.setString(2, id);
+			ps.executeUpdate();
+			result = true;
+		}
+		close(ps, conn);
+		return result;
+	}
+	
+	public void changeName(String id, String changeName) throws SQLException {
+		Connection conn = getConnect();
 		// 이름 변경 기능 구현 -> member 테이블에서 id로 새로운 이름으로 변경 (UPDATE)
+		
+		String query = "UPDATE member SET name = ? WHERE id = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		
+		ps.setString(1, changeName);
+		ps.setString(2, id);
+		ps.executeUpdate();
+		close(ps, conn);
 	}
 
 }
