1862e9edf5c05090fba67311c63626d090645f1a
yogu970126 <yogu970126@gmail.com>
2024-02-06 16:16:30 +0900

    웹 02_멤버 인코딩 필터
    - 한글 출력을 위해 utf-8로 인코딩 필터 생성
    - 생성한 필터는 xml에서 전체 경로가 거쳐 지나가게 설정

---
+++ Web/02_Member/src/com/kh/EncodingFilter.java
@@ -0,0 +1,24 @@
+package com.kh;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+public class EncodingFilter extends HttpFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// 한글처리 인코딩
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+
+		// 사전작업의 내용이 서버상의 다음 컴포넌트(servlet)에게 계속 적용되기 위한 작업
+		chain.doFilter(request, response);
+	}
+
+}
