52e24f9aecd8919b76c12bc16e3357bbe7ab4da7
yogu970126 <yogu970126@gmail.com>
2024-02-07 15:44:15 +0900

    JDBC 05 실습문제
    JDBC 05 실습문제
    - DAO 컨트롤러로 변환 전 코드 추가

--- JDBC/05_BookMVC2/src/com/kh/controller/BookController.java
+++ JDBC/05_BookMVC2/src/com/kh/controller/BookController.java
@@ -17,6 +17,27 @@ public class BookController {
 	
 	// 1. 전체 책 조회
 	public ArrayList<Book> printBookAll() {
+//		Connection conn = getConnect();
+//
+//		String query = "SELECT * FROM tb_book LEFT JOIN tb_publisher USING (pub_no)";
+//		PreparedStatement ps = conn.prepareStatement(query);
+//
+//		ResultSet rs = ps.executeQuery();
+//
+//		ArrayList<Book> list = new ArrayList<>();
+//		while(rs.next()) {
+//			Book book = new Book();
+//			book.setBkNo(rs.getInt("bk_no"));
+//			book.setBkTitle(rs.getString("bk_title"));
+//			book.setBkAuthor(rs.getString("bk_author"));
+//			book.setBkPrice(rs.getInt("bk_price"));
+//			book.setPublisher(new Publisher(rs.getInt("pub_no"), 
+//											rs.getString("pub_name"),
+//											rs.getString("phone")));
+//			list.add(book);
+//		}
+//		return list;
+		
 		try {
 			return dao.printBookAll();
 		} catch (SQLException e) {
@@ -27,6 +48,14 @@ public class BookController {
 	
 	// 2. 책 등록
 	public boolean registerBook(String title, String author) {
+//		Connection conn = getConnect();
+//		String query = "INSERT INTO tb_book(bk_title, bk_author) VALUES(?, ?)";
+//		PreparedStatement ps = conn.prepareStatement(query);
+//		ps.setString(1, title);
+//		ps.setString(2, author);
+//		ps.executeUpdate();
+//		close(ps, conn);
+		
 		try {
 			if(dao.registerBook(title, author)==1) {
 				return true;
@@ -39,6 +68,14 @@ public class BookController {
 	}
 	// 3. 책 삭제
 	public boolean sellBook(int no)  {
+//		Connection conn = getConnect();
+//		String query = "DELETE FROM tb_book WHERE bk_no=?";
+//		PreparedStatement ps = conn.prepareStatement(query);
+//		ps.setInt(1, no);
+//
+//		if(ps.executeUpdate() == 0) throw new SQLException();
+//		close(ps, conn);
+		
 		try {
 			if(dao.sellBook(no) == 1) return true;
 		} catch (SQLException e) {
