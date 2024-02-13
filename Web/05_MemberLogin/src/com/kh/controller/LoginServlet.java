830f96a059c5e650757e6daff0428e981f694c6d
yogu970126 <yogu970126@gmail.com>
2024-02-13 18:32:33 +0900

    웹 05_멤버 로그인 실습문제
    웹 05_멤버 로그인 실습문제 내 풀이
    - 로그인 서블릿

---
+++ Web/05_MemberLogin/src/com/kh/controller/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.kh.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.model.dao.MemberDAO;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 폼 값 받아오기
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		MemberDAO dao = new MemberDAO();
+		
+	}
+
+}
