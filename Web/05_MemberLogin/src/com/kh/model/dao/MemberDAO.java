d11d7e967916e822c37fb255d95b52eeb06214c4
yogu970126 <yogu970126@gmail.com>
2024-02-13 18:33:28 +0900

    웹 05_멤버 로그인 실습문제 내 풀이
    웹 05_멤버 로그인 실습문제 내 풀이
    - dao 모델

---
+++ Web/05_MemberLogin/src/com/kh/model/dao/MemberDAO.java
@@ -0,0 +1,70 @@
+package com.kh.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.kh.model.vo.Member;
+
+public class MemberDAO {
+	
+	public MemberDAO() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnect() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/kh", "root", "1234");
+	}
+	
+	public void close(PreparedStatement ps, Connection conn) throws SQLException {
+		if(ps!=null) ps.close();
+		if(conn!=null) conn.close();
+	}
+	
+	public void close(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException {
+		if(rs!=null) rs.close();
+		close(ps, conn);
+	}
+	
+	public int registerMember(Member m) throws SQLException {
+		Connection conn = getConnect();
+		String query = "INSERT INTO member VALUES(?, ?, ?)";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1, m.getId());
+		ps.setString(2, m.getPassword());
+		ps.setString(3, m.getName());
+		
+		int result = ps.executeUpdate();
+		close(ps, conn);
+		return result;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
