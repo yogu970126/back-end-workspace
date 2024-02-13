e1ce3077fc49ac0d09d8840a428b353bb5b093a9
yogu970126 <yogu970126@gmail.com>
2024-02-13 18:25:28 +0900

    웹 03_ 멤버MVC
    웹 03_ 멤버MVC
    - DAO 모델

--- Web/03_MemberMVC/src/com/kh/model/dao/MemberDAO.java
+++ Web/03_MemberMVC/src/com/kh/model/dao/MemberDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.kh.model.vo.Member;
 
@@ -45,4 +46,59 @@ public class MemberDAO {
 		close(ps, conn);
 		return result;
 	}
+	
+	public ArrayList<Member> showAllMember() throws SQLException {
+		Connection conn = getConnect();
+		String query = "SELECT * FROM member";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		
+		ArrayList<Member> list = new ArrayList<Member>();
+		while(rs.next()) {
+			list.add(new Member(rs.getString("id"),
+								rs.getString("password"), 
+								rs.getString("name")));
+		}
+		close(rs, ps, conn);
+		return list;
+	}
+
+	/*
+	public ArrayList<Member> searchMember(Member m) throws SQLException {
+		Connection conn = getConnect();
+		String query = "SELECT * FROM member WHERE name = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1, m.getName());
+		ResultSet rs = ps.executeQuery();
+		
+		ArrayList<Member> list = new ArrayList<Member>();
+		while(rs.next()) {
+			if(rs.getString("name").equals(m.getName())) {
+				list.add(new Member(rs.getString("id"),
+									rs.getString("password"),
+									rs.getString("name")));
+			}
+		}
+		close(rs,ps,conn);
+		return list;
+	} */
+	
+	// 강사님 풀이
+	public Member searchMember(String name) throws SQLException {
+		Connection conn = getConnect();
+		String query = "SELECT * FROM member WHERE name = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1, name);
+		ResultSet rs = ps.executeQuery();
+		
+		Member member = null;
+		if(rs.next()) {
+			member = new Member(rs.getString("id"),
+					   rs.getString("password"),
+					   rs.getString("name"));
+		}
+		close(rs,ps,conn);
+		return member;
+	}
+	
 }
