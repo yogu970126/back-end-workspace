a48959ed468c393abb4dacf8d4ccc216953102a0
yogu970126 <yogu970126@gmail.com>
2024-02-15 15:22:21 +0900

    배열
    공부한다고 잠시 수정

--- Java/05_Array/src/com/kh/array/A_Array.java
+++ Java/05_Array/src/com/kh/array/A_Array.java
@@ -189,14 +189,14 @@ public class A_Array {
 
 	public static void main(String[] args) {
 		A_Array a = new A_Array();
-//		a.method1();
+		a.method1();
 //		a.method2();
 //		a.method3();
 //		a.method4();
 //		a.method5();
 //		a.method6();
 //		a.method7();
-		a.method8();
+//		a.method8();
 		
 		
 			}
